28225ffcd3c6efdd13cd5621e6da0e4015e7cc0f
Derek DiLeo <derek@Dereks-Mac-mini.local>
2022-01-21 08:26:42 -0500

    Add user authentication/check  to chat screen.

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flash_chat_flutter/utilities/constants.dart';
 
 class ChatScreen extends StatefulWidget {
@@ -10,7 +12,26 @@ class ChatScreen extends StatefulWidget {
 
 class _ChatScreenState extends State<ChatScreen> {
   final _auth = FirebaseAuth.instance;
-  String signedInUser = '';
+  late User signedInUser;
+
+  @override
+  void initState() {
+    super.initState();
+    getCurrentUser();
+  }
+
+  void getCurrentUser() async {
+    try {
+      final user = await _auth.currentUser;
+      if (user != null) {
+        signedInUser = user;
+        print('User ID: ${user.uid}');
+        print(signedInUser.email);
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
