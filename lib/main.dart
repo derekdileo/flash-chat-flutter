2d480c7b70d0d011a06703c656acd5bdc7ff20c2
Derek DiLeo <derek@Dereks-Mac-mini.local>
2022-01-22 11:11:10 -0500

    Refactor main. Remove commented out code.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flash_chat_flutter/screens/welcome_screen.dart';
 import 'package:flash_chat_flutter/screens/login_screen.dart';
 import 'package:flash_chat_flutter/screens/registration_screen.dart';
@@ -12,18 +9,11 @@ void main() => runApp(FlashChat());
 class FlashChat extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        // theme: ThemeData.dark().copyWith(
-        //   textTheme: TextTheme(
-        //     bodyText1: TextStyle(color: Colors.black54),
-        //   ),
-        // ),
-        initialRoute: WelcomeScreen.id,
-        routes: {
-          WelcomeScreen.id: (context) => WelcomeScreen(),
-          RegistrationScreen.id: (context) => RegistrationScreen(),
-          LoginScreen.id: (context) => LoginScreen(),
-          ChatScreen.id: (context) => ChatScreen(),
-        });
+    return MaterialApp(initialRoute: WelcomeScreen.id, routes: {
+      WelcomeScreen.id: (context) => WelcomeScreen(),
+      RegistrationScreen.id: (context) => RegistrationScreen(),
+      LoginScreen.id: (context) => LoginScreen(),
+      ChatScreen.id: (context) => ChatScreen(),
+    });
   }
 }
